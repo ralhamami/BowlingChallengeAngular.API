6789f5d1c5abe320d091be552b829d8d562f4b46
Rayan Al-Hammami <ralhamami@gmail.com>
2023-11-03 18:46:35 -0500

    Added some comments to abstract and concrete frames.

--- Models/LastFrame.cs
+++ Models/LastFrame.cs
@@ -4,10 +4,12 @@
     {
         public override List<int?> Shots { get; set; } = new List<int?> { null, null, null };
 
+        //The last frame can have up to 30 pins knocked down if all strikes.
         public override int MaxPins => 30;
 
         public override void AddScore(int score)
         {
+            //Get index of next empty shot slot, and add the shot.
             int nextEmpty = Shots.IndexOf(null);
             Shots[nextEmpty] = score;
 
@@ -17,7 +19,12 @@
         public override void EvaluateFrame()
         {
             FrameTotalScore = Shots.Sum(s => s.GetValueOrDefault());
+            
+            //Last frame is considered complete if the third shot slot has a value,
+            //or if the first two shots don't add up to at least 10, meaning no bonus shot.
             IsComplete = Shots[Shots.Count - 1].HasValue || (Shots.Count(s=>s.HasValue) == 2 && Shots.Take(2).Sum(s=>s.GetValueOrDefault())<10);
+
+            //Score is considered final in last frame since additional points aren't awarded.
             IsScoreFinal = IsComplete;
         }
     }
--- Models/NormalFrame.cs
+++ Models/NormalFrame.cs
@@ -4,10 +4,12 @@
     {
         public override List<int?> Shots { get; set; } = new List<int?> { null, null };
 
+        //A normal frame can never have more than 10 pins knocked down.
         public override int MaxPins => 10;
 
         public override void AddScore(int score)
         {
+            //Get index of next empty shot slot, and add the shot.
             int nextEmpty = Shots.IndexOf(null);
             Shots[nextEmpty] = score;
 
@@ -16,14 +18,17 @@
 
         public override void EvaluateFrame()
         {
+            //If the first shot is 10, it's a strike.
             if (Shots[0].HasValue && Shots[0].Value == 10)
             {
                 IsStrike = true;
             }
+            //If the sum of the shots is 10, it's a spare.
             else if (Shots[0].GetValueOrDefault() + Shots[1].GetValueOrDefault() == 10)
             {
                 IsSpare = true;
             }
+            //Otherwise, it's an open frame, and the score is final since no additional points awarded.
             else if (Shots[1].HasValue)
             {
                 IsOpenFrame = true;
@@ -31,6 +36,8 @@
             }
 
             FrameTotalScore = Shots.Sum(s => s.GetValueOrDefault());
+            
+            //Frame is considered complete if first shot is a strike, or if second shot has a value.
             IsComplete = Shots[Shots.Count - 1].HasValue || (Shots[0].GetValueOrDefault() == 10);
         }
     }
